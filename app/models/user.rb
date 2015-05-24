6e5b233cf8a1e8b3976120db39e697b4184b7a84
Natt Eyre <terskaya.nv@gmail.com>
2015-05-24 20:42:30 +0300

    Added possibility to unfollow user with noticing, added page for a list of all users, did some refactoring with Dashboard to follow SOLID & Sandy Metz's rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,23 @@ class User < ActiveRecord::Base
 
   has_many :followed_user_relationships,
     foreign_key: :follower_id,
-    class_name: 'FollowingRelationsip'
+    class_name: 'FollowingRelationship'
   has_many :followed_users, through: :followed_user_relationships
 
   has_many :follower_relationships,
     foreign_key: :followed_user_id,
-    class_name: 'FollowingRelationsip'
+    class_name: 'FollowingRelationship'
   has_many :followers, through: :follower_relationships
+
+  def following? user
+    followed_user_ids.include? user.id
+  end
+
+  def follow user
+    followed_users << user
+  end
+
+  def unfollow user
+    followed_users.delete(user)
+  end
 end
